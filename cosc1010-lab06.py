ec1eb66e785ed69fafe4e4441afe03b37c397ce8
Jake Huggins <119003692+Siliconsim@users.noreply.github.com>
2024-10-15 18:36:14 +0000

    Finished Lab Assignment

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -79,22 +79,55 @@ print(len(random_string)) # Print out the size for reference
     # You will  need to add the letter to the dictionary on first occurrence 
     # Then increment its corresponding count 
 
-
 #Load all the elements into a dictionary
 #Will need to first declare a dictionary 
 
+alpha_values = {}
+
+for letter in random_string:
+    if not alpha_values.get(letter):
+        alpha_values[letter] = 1
+    else:
+        alpha_values[letter] += 1
+
 # Output: each letter and its corresponding occurrence in alphabetical order
+
+for key in sorted(alpha_values):
+    print(f"{key} : {alpha_values[key]}")
 
 print("*"*75)
 # Output which letter occurred the most 
+values = list(alpha_values.values())
 
 most_occurred = ""
 least_occurred = ""
+max_occurence, least_occurrence = values[0], values[0] 
+total_occurence = 0
+
+
+# Get the value of the first key in the dict for the initial value of max/min_occurrence
+#for value in alpha_values.values():
+#    max_occurence = value
+#    least_occurrence = value
+#    break
+
+for key in alpha_values:
+    total_occurence += alpha_values[key]
+    if alpha_values[key] >= max_occurence:
+        max_occurence = alpha_values[key]
+        most_occurred = key
+    elif not alpha_values[key] > least_occurrence:
+        least_occurred = key
+        least_occurrence = alpha_values[key]
 
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+print(f"The letter that occurred the least is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+
+for key in sorted(alpha_values):
+    percentage = (alpha_values[key] / total_occurence) * 100
+    print(f"Percentage of {key}: {round(percentage, 2)}%") 
